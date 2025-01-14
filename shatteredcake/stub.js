43e8faaa761fc45b6e54462712655b394a2d1cd3
notaero2 <tristantan123@yahoo.com.sg>
2025-01-14 12:39:11 +0800

    Revert "Delete shatteredcake directory"
    This reverts commit 8aa211cc80f221cf9c1f4060e82f0da3400d7689.

---
+++ shatteredcake/stub.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'calculateArea' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER w
+ *  2. 2D_INTEGER_ARRAY dimensions
+ */
+
+function calculateArea(w, dimensions) {
+    // Write your code here
+
+}
+
+function main() {
+    const ws = process.stdout;
+
+    const w = parseInt(readLine().trim(), 10);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    let dimensions = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        dimensions[i] = readLine().replace(/\s+$/g, '').split(' ').map(dimensionsTemp => parseInt(dimensionsTemp, 10));
+    }
+
+    const result = calculateArea(w, dimensions);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
